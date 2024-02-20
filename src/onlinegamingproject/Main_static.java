27f5d4f2df0589ebb75942a5a8a63fbb3ad60ad3
amiraBenamer20 <amira.rayane.benamer@gmail.com>
2024-02-20 09:59:49 +0100

    .gitignore updates

--- src/onlinegamingproject/Main_static.java
+++ src/onlinegamingproject/Main_static.java
@@ -19,12 +19,12 @@ public class Main_static
     {
         
         
-        String root = "E:\\";//make sure to replace D:\\ by the current directory where the code exists
+        String root = "C:\\Users\\amira\\";//make sure to replace D:\\ by the current directory where the code exists
         String path = root+"GamingSim\\Data_Static\\"; 
-        int configurations[]={20};//20,40,60,80,100,250,500,750,1000,1250,1500,1750,2000,3000,4000,5000};
+        int configurations[]={20,40,60,80,100,250,500,750,1000,1250,1500,1750,2000,3000,4000,5000};
      
-        int instances=100;//Number of instances for each configuration
-        int start=99;
+        int instances=5;//Number of instances for each configuration
+        int start=0;
         int rerun=1;
         
         int thresholds[]={0};//percent of players with tolerated latency violations
